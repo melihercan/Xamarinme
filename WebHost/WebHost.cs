c88062d19eef2c83a02114ec19e3c0051abaa888
Melih Ercan <melihercan@gmail.com>
2021-03-24 14:00:08 +0300

    Adding WebHost library.

---
+++ WebHost/WebHost.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using XamarinmeWebHost;
+
+namespace Xamarinme
+{
+    public class WebHost
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                })
+                .UseEmbedIoServer()
+                .ConfigureWebHost((hostContext, app) =>
+                {
+                    app.Run(async (context) =>
+                    {
+                        await context.Response.WriteAsync("Hello World!");
+                    });
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            var s = host.Services;
+
+            await host.RunAsync();
+        }
+    }
+}
---
+++ Xamarin.Essentials.FileSystem.Extensions/FileSystemExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Xamarin.Essentials
+{
+    public partial class FileSystem
+    {
+        /// <summary>
+        /// Extension method for Xamarin.Essentials to open a file from embedded resource.
+        /// </summary>
+        /// <param name="prefix">
+        ///     Prefix to embedded resource file.
+        ///     Format: <default namespace>.<nested folders that contains the file each separated with a dot>
+        ///     Examples:
+        ///          default namespace: "MyApp",     file is on root folder         => Prefix = "MyApp"
+        ///          default namespace: "MyApp",     file is on "res" folder        => Prefix = "MyApp.res"
+        ///          default namespace: "MyApp",     file is on "res/x/y" folder    => Prefix = "MyApp.res.x.y"
+        ///          default namespace: "MyApp.iOS", file is on "res/x/y" folder    => Prefix = "MyApp.iOS.res.x.y"
+        /// </param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static Stream OpenEmbeddedResourceFile(string prefix, string fileName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var name = $"{prefix}.{fileName}";
+            var stream = assembly.GetManifestResourceStream(name);
+            return stream;
+        }
+    }
+}
