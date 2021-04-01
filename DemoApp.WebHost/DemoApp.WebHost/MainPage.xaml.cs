ca6a11127dccad72bcd5f695cd2d87224c4fbde1
Melih Ercan <melihercan@gmail.com>
2021-04-01 11:01:34 +0300

    Showing URL on Main page.

--- DemoApp.WebHost/DemoApp.WebHost/MainPage.xaml.cs
+++ DemoApp.WebHost/DemoApp.WebHost/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarinme;
 
 namespace DemoApp.WebHost
 {
@@ -13,6 +14,11 @@ namespace DemoApp.WebHost
         public MainPage()
         {
             InitializeComponent();
+
+            var ipString = CrossIpAddress.Current.Get();
+            Ip.Text = ipString;
+            Url.Text = $"http://{ipString}:5000";
+
         }
     }
 }
---
+++ DemoApp.WebHost/DemoApp.WebHost/README_XAMARINME.txt
@@ -0,0 +1,4 @@
+﻿Normally this kind of apps will include the 'WebHostPatch' NuGet that overrides the Microsoft.Extensions.Primitives
+assembly.
+If project is included instaed of NuGet (debugging for example), iOS build complains that it cannot find v2.2.0 of
+primitives. Therefore use debugging only on Android with this format.
